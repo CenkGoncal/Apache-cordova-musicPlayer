50231fba5379472cfa00eb2d44c8c84711b51b1c
CenkGoncal <cenk_manga@hotmail.com>
2015-08-29 20:45:51 +0300

    Revert "version 1.0"
    This reverts commit 0906dda2b203a754fb3bf49f9151de564a2ff4aa.

--- BlankCordovaApp2/BlankCordovaApp2/bld/Debug/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\cenk\Desktop\BlankCordovaApp2\BlankCordovaApp2\bld\Debug\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
-C:\Users\cenk\Desktop\BlankCordovaApp2\BlankCordovaApp2\bld\Debug\platforms\android\CordovaLib\AndroidManifest.xml \
--- BlankCordovaApp2/BlankCordovaApp2/bld/Debug/platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\cenk\Desktop\BlankCordovaApp2\BlankCordovaApp2\bld\Debug\platforms\android\ant-build\AndroidManifest.xml : \
-C:\Users\cenk\Desktop\BlankCordovaApp2\BlankCordovaApp2\bld\Debug\platforms\android\AndroidManifest.xml \
