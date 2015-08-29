0906dda2b203a754fb3bf49f9151de564a2ff4aa
CenkGoncal <cenk_manga@hotmail.com>
2015-08-29 20:32:21 +0300

    version 1.0
    online radio is completed

---
+++ BlankCordovaApp2/BlankCordovaApp2/bld/Debug/platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\cenk\Desktop\BlankCordovaApp2\BlankCordovaApp2\bld\Debug\platforms\android\ant-build\AndroidManifest.xml : \
+C:\Users\cenk\Desktop\BlankCordovaApp2\BlankCordovaApp2\bld\Debug\platforms\android\AndroidManifest.xml \
